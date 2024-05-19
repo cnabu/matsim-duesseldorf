f401738924a7c0c8b7b9a2bd40efc6309215a2e5
cnabu <cnabu@leeds.ac.uk>
2024-05-19 13:34:10 +0100

    added changes to 5 bridges

---
+++ src/main/java/org/matsim/prepare/LimitedAccessBridge.java
@@ -0,0 +1,30 @@
+package org.matsim.prepare;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.utils.collections.CollectionUtils;
+
+import java.util.Set;
+
+public class LimitedAccessBridge {
+
+	public static void main(String[] args){
+
+		final String[] mode = {"bike"};
+
+		var network = NetworkUtils.readNetwork("C:/Users/arsal/Downloads/duesseldorf-1pct-dc_114.output_network.xml");
+
+		for(Link link : network.getLinks().values()){
+			if (link.getId().equals(Id.createLinkId("90115782"))){
+				link.setAllowedModes(CollectionUtils.stringArrayToSet(mode));
+
+
+			}
+
+			NetworkUtils.writeNetwork(network,"C:/Users/arsal/Downloads/changed-network-v1.7-network-with-pt.xml.gz");
+
+		}
+
+	}
+}
